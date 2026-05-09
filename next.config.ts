22b3f3181f4a33174e1d6513929fb7dea4f5a093
freddy208 <kfreddypatient@gmail.com>
2026-05-09 11:47:08 +0100

    Mise en prod v1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.1.125', '192.168.217.172', 'localhost', '127.0.0.1'],
+  allowedDevOrigins: ['https://factura-cm.vercel.app/s','192.168.1.125', '192.168.217.172', 'localhost', '127.0.0.1'],
   turbopack: {},
 }
 
