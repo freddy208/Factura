01aef910347ef5b96134e3c696e0a11dcac19676
freddy208 <kfreddypatient@gmail.com>
2026-05-09 13:12:31 +0100

    fix: remove next-pwa incompatible with Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['https://factura-cm.vercel.app/s','192.168.1.125', '192.168.217.172', 'localhost', '127.0.0.1'],
-  turbopack: {},
+  allowedDevOrigins: ['192.168.1.125', '192.168.217.172', 'localhost', '127.0.0.1'],
 }
 
-// PWA seulement en production
-if (process.env.NODE_ENV === 'production') {
-  const withPWA = require('next-pwa')({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  })
-  module.exports = withPWA(nextConfig)
-} else {
-  module.exports = nextConfig
-}
+module.exports = nextConfig
